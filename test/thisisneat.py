cf398419c7bd12d29962e737ec9882fb4b563be4
sauravpanchal <saurav.panchal.08@gmail.com>
2022-03-05 10:42:09 +0530

    Create/Update

---
+++ test/thisisneat.py
@@ -0,0 +1,87 @@
+"""
+Single-pole balancing experiment using a feed-forward neural network.
+"""
+
+import multiprocessing
+import os
+import pickle
+import neat
+# import cart_pole
+import gym
+import numpy as np
+
+runs_per_net = 2
+# simulation_seconds = 60.0
+
+
+# Use the NN network phenotype and the discrete actuator force function.
+def eval_genome(genome, config):
+    net = neat.nn.FeedForwardNetwork.create(genome, config)
+
+    fitnesses = []
+
+    for runs in range(runs_per_net):
+        env = gym.make("CartPole-v1")
+
+        # sim = cart_pole.CartPole()
+        observation = env.reset()
+
+
+        # Run the given simulation for up to num_steps time steps.
+        fitness = 0.0
+        done = False
+        while not done:
+            # inputs = sim.get_scaled_state()
+            action = np.argmax(net.activate(observation))
+
+            observation, reward, done, info = env.step(action)
+
+
+            # Apply action to the simulated cart-pole
+            # force = cart_pole.discrete_actuator_force(action)
+            # sim.step(force)
+
+            # Stop if the network fails to keep the cart within the position or angle limits.
+            # The per-run fitness is the number of time steps the network can balance the pole
+            # without exceeding these limits.
+            # if abs(sim.x) >= sim.position_limit or abs(sim.theta) >= sim.angle_limit_radians:
+                # break
+
+            fitness += reward
+
+        fitnesses.append(fitness)
+
+    # The genome's fitness is its worst performance across all runs.
+    return np.mean(fitnesses)
+
+
+def eval_genomes(genomes, config):
+    for genome_id, genome in genomes:
+        genome.fitness = eval_genome(genome, config)
+
+
+def run():
+    # Load the config file, which is assumed to live in
+    # the same directory as this script.
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, 'config_feedforward.txt')
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         config_path)
+
+    pop = neat.Population(config)
+    stats = neat.StatisticsReporter()
+    pop.add_reporter(stats)
+    pop.add_reporter(neat.StdOutReporter(True))
+
+    pe = neat.ParallelEvaluator(multiprocessing.cpu_count(), eval_genome)
+    winner = pop.run(pe.evaluate)
+
+    # Save the winner.
+    with open('winner_feedforward', 'wb') as f:
+        pickle.dump(winner, f)
+
+    print(winner)
+
+if __name__ == '__main__':
+    run()
